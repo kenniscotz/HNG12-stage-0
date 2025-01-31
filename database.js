baa770d4bd93e0a251891fbc24d0944b678db58c
kenniscotz <74797589+kenniscotz@users.noreply.github.com>
2025-01-31 12:36:16 +0100

    Update database.js

--- database.js
+++ database.js
@@ -2,5 +2,5 @@
 export const responseMessage = {
     email: "kenniscotzotti@gmail.com",
    current_datetime: new Date().toISOString(),
-  github_URL: "https://github.com/kenniscotz/HNG12-stage-0",
+  github_url: "https://github.com/kenniscotz/HNG12-stage-0",
 };
